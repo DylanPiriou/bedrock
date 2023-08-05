4d0b849c6146e119ddde7a2d7fe141460bc7aa0f
DylanPiriou <dylan.piriou@hotmail.com>
2023-08-05 15:57:02 +0200

    feat: tailwindcss added

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{php,html,js}","./web/app/themes/*.{php,html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
